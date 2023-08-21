c46b85cf49342dc756242494f2e70ff075ce3422
Aarón Fas <yo@aaron.com.es>
2023-08-21 08:32:58 +0200

    Do not react to your own pull requests.

--- scripts/auto-react.js
+++ scripts/auto-react.js
@@ -25,7 +25,14 @@ const isMainPrUrl = (url) => url.match(/https:\/\/github.com\/.*\/pull\/\d+$/);
 
 // Handle user navigating out and in of the main PR page.
 navigation.addEventListener("navigate", e => {
-  console.log(e);
+  const user_login = document.querySelector('meta[name="user-login"]').content;
+  const author_login = document.querySelector('meta[name="og:author:username"]').content;
+
+  if (user_login === author_login) {
+    // Don't react to your own PRs.
+    return;
+  }
+
   // If user navigates outside of main PR page, stop the timeout.
   if (!isMainPrUrl(e.destination.url)) {
     clearTimeout(timeoutId);
